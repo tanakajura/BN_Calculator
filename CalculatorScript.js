40f328dbb1fb7150e7d69717d97e7b4803cc92c9
tanakajura <105241280+tanakajura@users.noreply.github.com>
2022-07-26 17:16:22 +0200

    Update CalculatorScript.js

--- CalculatorScript.js
+++ CalculatorScript.js
@@ -18,7 +18,26 @@ function division(num1, num2){
     return ans;
 }
 
+function operate(operator, num1,num2){
+    let ans = 0;
+    switch(operator){
+        case "+":
+            ans = addition(num1,num2);
+            return ans;
+        case "-":
+            ans = subtraction(num1,num2);
+            return ans;
+        case "*":
+            ans = multiplication(num1,num2);
+            return ans;
+        case "/":
+            ans = division(num1,num2);
+            return ans;
+    }
+}
+
 let testNum1 = parseInt(prompt("Enter number 1","0"));
 let testNum2 = parseInt(prompt("Enter number 2","0"));
-
-console.log(division(testNum1,testNum2))
+let testOperator = prompt("Enter Operator plus: \"+\" minus: \"-\" multiply: \"*\" or divide: \"-\"","+");
+ 
+console.log(operate(testNum1,testNum2));
